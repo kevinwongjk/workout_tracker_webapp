6b9cc45e57bd779d65f728bd7127002f51a9ae52
Kevin Wong <kevinwong.jk@gmail.com>
2024-05-16 10:34:28 -0700

    Add simple middleware

--- gradual-gains/server.js
+++ gradual-gains/server.js
@@ -6,6 +6,12 @@ const express = require('express')
 // initialize express app
 const app = express()
 
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()  
+})
+
 // routes
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to the app'})
