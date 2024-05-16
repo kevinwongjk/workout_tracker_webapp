8578e54975ecfc4c7a8840d9c917019ccb10e672
Kevin Wong <kevinwong.jk@gmail.com>
2024-05-16 09:42:26 -0700

    Modify server.js with basic root route

--- gradual-gains/server.js
+++ gradual-gains/server.js
@@ -1,6 +1,14 @@
+// import express library
 const express = require('express')
+// initialize express app
 const app = express()
 
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+
+// listen for requests
 app.listen(4000, () => {
     console.log('Listening on port 4000!')
 })
