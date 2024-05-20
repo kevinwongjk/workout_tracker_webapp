15b1e557c5add44b44f534c2608347995578de97
Kevin Wong <kevinwong.jk@gmail.com>
2024-05-19 17:32:56 -0700

    Modify delete button from word to icon

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -20,7 +20,7 @@ const WorkoutDetails = ({ workout }) => {
             <p><strong>Load (lb): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
-            <span onClick={handleClick}>delete</span>
+            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
         </div>
     )
 }
