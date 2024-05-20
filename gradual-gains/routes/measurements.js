9dfca25c68514b89967085a6402ebdae50bd4d2e
Kevin Wong <kevinwong.jk@gmail.com>
2024-05-19 18:44:18 -0700

    Create measurements.js to contain routes for all measurements

---
+++ gradual-gains/routes/measurements.js
@@ -0,0 +1,68 @@
+const express = require('express')
+// const {
+//     createWorkout,
+//     getWorkouts,
+//     getWorkout,
+//     deleteWorkout,
+//     updateWorkout
+// } = require('../controllers/workoutController')
+// const {
+//     getMeasurements,
+//     createWeightMeasurement,
+//     getWeightMeasurement,
+//     deleteWeightMeasurement
+// } = require('../controllers/measurementController')
+
+const router = express.Router()
+
+// GET all measurements
+// router.get('/', getMeasurements)
+router.get('/', (req, res) => {
+    res.json({"mssg": "hello"})
+})
+
+// POST a new weight measurement
+router.post('/weight/', createWeightMeasurement)
+
+// // GET a single workout
+router.get('/weight/:id', getWeightMeasurement)
+
+// DELETE a weight measurement
+router.delete('/weight/:id', deleteWeightMeasurement)
+
+// POST a new body fat measurement
+
+// Post a new neck measurement
+
+// Post a new shoulders measurement
+// Post a new chest measurement
+// Post a new left bicep measurement
+// Post a new right bicep measurement
+// Post a new left forearm measurement
+// Post a new right forearm measurement
+// Post a new upper abs measurement
+// Post a new waist measurement
+// Post a new lower abs measurement
+// Post a new hips measurement
+// Post a new left thigh measurement
+// Post a new right thigh measurement
+// Post a new left calf measurement
+// Post a new right calf measurement
+
+
+// // GET all workouts
+// router.get('/', getWorkouts)
+
+// // GET a single workout
+// router.get('/:id', getWorkout)
+
+// // POST a new workout
+// router.post('/', createWorkout)
+
+// // DELETE a workout
+// router.delete('/:id', deleteWorkout)
+
+// // UPDATE a workout
+// router.patch('/:id', updateWorkout)
+
+module.exports = router
